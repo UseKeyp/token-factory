48e15f2d605e64be34603ccd23c1822eef73a919
Hunter King <hking6776@gmail.com>
2023-04-05 15:45:53 -0500

    add token folder

--- src/TokenFactory.sol
+++ src/TokenFactory.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract TokenFactory {}
+contract TokenFactory {
+    function deployERC20() external returns (address) {}
+
+    function deployERC721() external returns (address) {}
+
+    function deployERC1155() external returns (address) {}
+}
